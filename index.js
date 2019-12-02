74cb47dc5a25ad5fa091319656731a3244278d45
Sudhan-at-575760765279 <b.j.ponsudhan@gmail.com>
2019-12-02 18:36:50 +0530

    scale according to viewport

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        parent: 'phaser-example',
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 800,
+        height: 600
+    },
+    
     physics: {
         default: 'arcade',
         arcade: {
