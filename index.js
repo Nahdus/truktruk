38d94f6d48e8a4b8586b5f15fc3440c2ede999db
Sudhan-at-575760765279 <b.j.ponsudhan@gmail.com>
2019-12-11 18:05:25 +0530

    shooting bug fix

--- index.js
+++ index.js
@@ -247,7 +247,7 @@ class intro extends Phaser.Scene {
        this.player.setVelocityX(0);
        if (!playerDead)  {
    
-           if (this.cursors.right.isDown||(this.input.pointer1.isDown && this.input.pointer1.x>window.innerWidth/2))
+           if (this.cursors.right.isDown && !this.input.pointer2.isDown&& !this.spacebar.isDown && !playerDead)
          {
              console.log("right")
             this.player.setVelocityX(160);
